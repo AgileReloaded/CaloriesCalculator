c96b3a1902071a76c4c3f6ef1eb71da0c0d23649
Stefano <stefano.leli@gmail.com>
2016-06-02 23:17:32 +0200

    aggiornato project a vs2015 e Ms Unit Test Framework

--- test/CaloriesCalculatorTest.cs
+++ test/CaloriesCalculatorTest.cs
@@ -2,89 +2,99 @@ namespace Kata
 {
 
     using System;
-    using NUnit.Framework;
     using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
-    [TestFixture]
-    public class CaloriesCalculatorTest {
+    [TestClass]
+    public class CaloriesCalculatorTest
+    {
 
-	    private Person tom;
-	    private Food apple;
+        private Person tom;
+        private Food apple;
         private List<Food> foods;
 
-	    [SetUp]
-	    public void setUp()
-	    {
-		    tom = new Person("tom", 72 );
-		    apple = new Food("apple", 0.2);
+        [TestInitialize]
+        public void setUp()
+        {
+            tom = new Person("tom", 72);
+            apple = new Food("apple", 0.2);
             foods = new List<Food>();
-	    }
-    	
-	    [Test]
-        [ExpectedException]
-	    public void shouldThrowAnExceptionWhenAFoodDoesNotHaveAName()  {
-		    Food foodWithoutName = new Food("", 0.0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+        public void shouldThrowAnExceptionWhenAFoodDoesNotHaveAName()
+        {
+            Food foodWithoutName = new Food("", 0.0);
             foods.Add(foodWithoutName);
             new CaloriesCalculator(tom, foods).result();
-	    }
+        }
 
-        [Test]
-        [ExpectedException]
-	    public void shouldThrowAnExceptionWhenWeightIsOverOneThousandKg()  {
-		    Person notHuman = new Person("any", 1001);
+        [TestMethod]
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+        public void shouldThrowAnExceptionWhenWeightIsOverOneThousandKg()
+        {
+            Person notHuman = new Person("any", 1001);
             foods.Add(apple);
             new CaloriesCalculator(notHuman, foods).result();
-	    }
-    	
-	    [Test]
-	    public void noFoodNoCalories()  {
-		    Food air = new Food("air", 0.0);
+        }
+
+        [TestMethod]
+        public void noFoodNoCalories()
+        {
+            Food air = new Food("air", 0.0);
             foods.Add(air);
             String actual = new CaloriesCalculator(tom, foods).result();
             Assert.IsTrue(actual.Contains("Diet Report for tom:\nair - 0\nTotal: 0 kcal"));
-	    }
-    	
-	    [Test]
-	    public void shouldDecreaseCaloriesWhenEatingTheMagicPill()  {
-		    Food magicPill = new Food("magicPill", 0.01);
+        }
+
+        [TestMethod]
+        public void shouldDecreaseCaloriesWhenEatingTheMagicPill()
+        {
+            Food magicPill = new Food("magicPill", 0.01);
             foods.Add(magicPill);
-		    String actual = new CaloriesCalculator(tom, foods).result();
+            String actual = new CaloriesCalculator(tom, foods).result();
             Assert.IsTrue(actual.Contains("Diet Report for tom:\nmagicPill - 0,01\nTotal: -10 kcal"));
-	    }
-    	
-	    [Test]
-	    public void howMuchJoggingToBurnCalories()  {
+        }
+
+        [TestMethod]
+        public void howMuchJoggingToBurnCalories()
+        {
             foods.Add(apple);
             Assert.IsTrue(reportForNormalWeight().Contains("kilometers to be run: " + 90 * Person.MEDIUM_SIZE / 30));
             Assert.IsTrue(reportForFatPeople().Contains("kilometers to be run: " + 90 * Person.FAT / 30));
             Assert.IsTrue(reportForSlimPeople().Contains("kilometers to be run: " + 90 * Person.SLIM / 30));
-	    }
-    	
-	    private String reportForSlimPeople()  {
-		    Person fab = new Person("fab", 45);            
+        }
+
+        private String reportForSlimPeople()
+        {
+            Person fab = new Person("fab", 45);
             return new CaloriesCalculator(fab, foods).result();
-	    }
+        }
 
-	    private String reportForFatPeople()  {
-		    Person bob = new Person("bob", 150);            
+        private String reportForFatPeople()
+        {
+            Person bob = new Person("bob", 150);
             return new CaloriesCalculator(bob, foods).result();
-	    }
+        }
 
-	    private String reportForNormalWeight()  {            
+        private String reportForNormalWeight()
+        {
             return new CaloriesCalculator(tom, foods).result();
-	    }
+        }
 
-	    [Test]
-	    public void appleCalories()  {
-		    Food apple = new Food("apple", 0.2);
+        [TestMethod]
+        public void appleCalories()
+        {
+            Food apple = new Food("apple", 0.2);
             foods.Add(apple);
             String actual = new CaloriesCalculator(tom, foods).result();
             Assert.IsTrue(actual.Contains("Diet Report for tom:\napple - 0,2\nTotal: 30 kcal"));
-            foods.Add(apple);               
-		    actual = new CaloriesCalculator(tom, foods).result();
+            foods.Add(apple);
+            actual = new CaloriesCalculator(tom, foods).result();
             Assert.IsTrue(actual.Contains("Diet Report for tom:\napple - 0,2\napple - 0,2\nTotal: 60 kcal"));
-	    }
-    		    
+        }
+
     }
 }
